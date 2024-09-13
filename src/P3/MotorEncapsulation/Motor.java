c27e813669b21bf84fa1ef4aef87eaec5795b7d7
dedybayu <dedybayusetiawan5@gmail.com>
2024-09-13 14:00:54 +0700

    P3_Percobaan3

--- src/P3/MotorEncapsulation/Motor.java
+++ src/P3/MotorEncapsulation/Motor.java
@@ -1,8 +1,35 @@
 package P3.MotorEncapsulation;
 
 public class Motor {
-    public int kecepatan = 0;
-    public boolean kontakOn = false;
+    private int kecepatan = 0;
+    private boolean kontakOn = false;
+
+    public void nyalakanMesin(){
+        kontakOn = true;
+    }
+    public void matikanMesin(){
+        kontakOn = false;
+        kecepatan = 0;
+    }
+    public void tambahKecepatan(){
+        if (kontakOn == true) {
+            if (kecepatan < 100) {
+                kecepatan += 5;
+            } else {
+                System.out.println("Kecepatan tidak dapat ditambah karena Sudah Maksimal (100)");
+            }
+            
+        } else {
+            System.out.println("Kecepatan tidak dapat ditambah karena Mesin OFF/n");
+        }
+    }
+    public void kurangiKecepatan(){
+        if (kontakOn == true) {
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak dapat dikurangi karena Mesin OFF\n");
+        }
+    }
 
     public void printStatus(){
         if (kontakOn == true) {
