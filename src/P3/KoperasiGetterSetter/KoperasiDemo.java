7cb306bc34ad589cf4a764596a76a2c5b17aed7a
dedybayu <dedybayusetiawan5@gmail.com>
2024-09-13 14:11:40 +0700

    P3_Percobaan3

---
+++ src/P3/KoperasiGetterSetter/KoperasiDemo.java
@@ -0,0 +1,15 @@
+package P3.KoperasiGetterSetter;
+
+public class KoperasiDemo {
+    public static void main(String[] args) {
+        Anggota anggota1 = new Anggota();
+        anggota1.setNama("Budi Setiawan");
+        anggota1.setAlamat("Jalan Soekarno Hatta no 10");
+        anggota1.setor(100000);
+        System.out.println("Simpanan " + anggota1.getNama() + ": " + anggota1.getSimpanan());
+
+        anggota1.pinjam(5000);
+        System.out.println("Simpanan " + anggota1.getNama() + ": " + anggota1.getSimpanan());
+
+    }
+}
